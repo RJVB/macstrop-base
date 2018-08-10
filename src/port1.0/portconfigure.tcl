0c81c0aa2b3dcf4999bca9161b07ddc182f4d9b8
René J.V. Bertin <rjvbertin@gmail.com>
2018-08-10 17:28:47 +0200

    /opt/local/site-ports/sysutils/MacPorts/files/patch-allow-clazy-and-gcclibcxx.diff

--- src/port1.0/portconfigure.tcl
+++ src/port1.0/portconfigure.tcl
@@ -74,7 +74,9 @@ option_proc configure.objcxxflags portconfigure::stdlib_trace
 proc portconfigure::should_add_stdlib {} {
     set has_stdlib [expr {[option configure.cxx_stdlib] ne ""}]
     set is_clang [string match *clang* [option configure.cxx]]
-    return [expr {$has_stdlib && $is_clang}]
+    set is_clazy [string match *clazy* [option configure.cxx]]
+    set is_gccok  [string match {*g*-mp-[7]*} [option configure.cxx]]
+    return [expr {$has_stdlib && ($is_clang || $is_clazy || $is_gccok)}]
 }
 proc portconfigure::should_add_cxx_abi {} {
     set is_oldos [expr {[option os.platform] eq "darwin" && [option os.major] < 10}]
@@ -285,6 +287,7 @@ proc portconfigure::configure_start {args} {
         {^llvm-gcc-4\.2$}                   {Xcode LLVM-GCC 4.2}
         {^macports-clang$}                  {MacPorts Clang (port select)}
         {^macports-clang-(\d+\.\d+)$}       {MacPorts Clang %s}
+        {^macports-clazy-(\d+\.\d+)$}       {MacPorts Clazy %s}
         {^macports-dragonegg-(\d+\.\d+)$}   {MacPorts DragonEgg %s}
         {^macports-dragonegg-(\d+\.\d+)-gcc-(\d+\.\d+)$}
             {MacPorts DragonEgg %s with GCC %s}
@@ -485,6 +488,7 @@ proc portconfigure::compiler_port_name {compiler} {
     set valid_compiler_ports {
         {^apple-gcc-(\d+)\.(\d+)$}                          {apple-gcc%s%s}
         {^macports-clang-(\d+\.\d+)$}                       {clang-%s}
+        {^macports-clazy-(\d+\.\d+)$}                       {clazy-%s}
         {^macports-dragonegg-(\d+\.\d+)(-gcc-\d+\.\d+)?$}   {dragonegg-%s%s}
         {^macports-(llvm-)?gcc-(\d+)(?:\.(\d+))?$}          {%sgcc%s%s}
     }
@@ -701,6 +705,16 @@ proc portconfigure::configure_get_compiler {type {compiler {}}} {
             objc    { return ${prefix}/bin/clang${suffix} }
             cxx     -
             objcxx  { return ${prefix}/bin/clang++${suffix} }
+        }
+    } elseif {[regexp {^macports-clazy(-\d+\.\d+)?$} $compiler -> suffix]} {
+        if {$suffix ne ""} {
+            set suffix "-mp${suffix}"
+        }
+        switch $type {
+            cc      -
+            objc    { return ${prefix}/bin/cclazy${suffix} }
+            cxx     -
+            objcxx  { return ${prefix}/bin/clazy${suffix} }
         }
     } elseif {[regexp {^macports-dragonegg(-\d+\.\d+)(?:-gcc(-\d+\.\d+))?$} $compiler \
                 -> infix suffix]} {
