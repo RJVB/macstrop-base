c2e4698bfd5f8a515a548dae3d6a6cb57a28aaa9
René Bertin <rjvbertin@gmail.com>
2024-05-30 13:59:51 +0200

    patch-truncate-logfile.diff

--- src/macports1.0/macports.tcl
+++ src/macports1.0/macports.tcl
@@ -210,8 +210,8 @@ proc macports::ch_logging {mport} {
 
     set ::debuglogname $logname
 
-    # Append to the file if it already exists
-    set ::debuglog [open $::debuglogname a]
+    # Truncate to the file if it already exists
+    set ::debuglog [open $::debuglogname w]
     puts $::debuglog version:1
 
     ui_debug "Starting logging for $portname @$portinfo(version)_$portinfo(revision)$portinfo(canonical_active_variants)"
